c75d6f4ed5a318b012a2d6ecfcd5c4d894b6530f
Harry <naingaunglu01@gmail.com>
2025-06-26 00:06:21 +0700

    implement: LogStore and OnMemoryLogStore artifacts

--- proxy/log_store.go
+++ proxy/log_store.go
@@ -1,5 +1,19 @@
 package proxy
 
-func NewLogStore() {
+import "net/http"
 
+type LogStore interface {
+	Save(request *http.Request, response *http.Response)
+}
+
+type OnMemoryLogStore struct {
+	Logs []string
+}
+
+func (s *OnMemoryLogStore) Save(request *http.Request, response *http.Response) {
+	s.Logs = append(s.Logs, request.Method+" ", request.URL.Path)
+}
+
+func NewLogStore() LogStore {
+	return &OnMemoryLogStore{}
 }
