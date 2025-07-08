3667e19b48d44957923b09701a567845e8993d58
Harry <naingaunglu01@gmail.com>
2025-07-09 00:20:52 +0700

    implement: response serializer

--- proxy/cache_helper.go
+++ proxy/cache_helper.go
@@ -1,6 +1,8 @@
 package proxy
 
 import (
+	"bufio"
+	"bytes"
 	"net/http"
 	"strings"
 )
@@ -15,4 +17,24 @@ func UniqueKeyForRequest(request http.Request) string {
 		request.URL.String(),
 	}
 	return strings.Join(keySegments, separator)
+}
+
+func SerializeResponse(response http.Response) string {
+	// body, err := io.ReadAll(response.Body)
+	// defer response.Body.Close()
+
+	// if err != nil {
+	// log.Fatalf("Error while reading response body: %v", err)
+	// }
+
+	buffer := bytes.Buffer{}
+	response.Write(&buffer)
+
+	return buffer.String()
+}
+
+func ResponseFromString(input string) http.Response {
+	reader := bytes.NewReader([]byte(input))
+	response, _ := http.ReadResponse(bufio.NewReader(reader), nil)
+	return *response
 }
